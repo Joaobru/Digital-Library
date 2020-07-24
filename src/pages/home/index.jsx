cdbc01b13e09f881f1d1d632d7dac6faa22d9ec6
Felipe <felipe.almeidaa14@gmail.com>
2020-07-24 19:51:50 -0300

    carrossel feito

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -57,7 +57,7 @@ const tutorialSteps = [
   {
     label: "Goč, Serbia",
     imgPath:
-      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=1000&h=250&q=60",
+      "https://lojasaraiva.vteximg.com.br/arquivos/ids/20923455/1617_tv_complete-sua-colecao.png?v=637305932733230000",
   },
 ];
 
